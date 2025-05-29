31772b328a4bbdf984ce4bb34d6d9002350472f9
0xvileness <thatonedude9773@gmail.com>
2025-05-29 02:46:28 -0400

    Update lookup.py

--- validator.py
+++ validator.py
@@ -1,7 +1,9 @@
-from time import time
-from numpy import number
 import phonenumbers
-from phonenumbers import timezone, geocoder, carrier
+from phonenumbers import timezone
+from phonenumbers import geocoder
+from phonenumbers import carrier
+
+
 
 print()
 print(" ██▒   █▓ ██▓ ██▓    ▓█████  ")
@@ -15,15 +17,29 @@ print("    ░░   ▒ ░  ░ ░      ░   ")
 print("      ░   ░      ░  ░   ░  ░ ")
 print("    ░                      ")
 print("      Made By @Oxycrime ")
+print()
 
+# Enter phone number with the country code
 
-number = input("Enter your number: ")
-phone = phonenumbers.parse(number)
-time = timezone.time_zones_for_number(phone)
-car =  carrier.name_for_number(phone, "en")
-reg =  geocoder.description_for_number(phone, "en")
+number = input("Enter phone number with country code : ")
 
-print (phone)
-print(time)
-print (car)
-print(reg)
+# Parsing String to the Phone number
+phone_number = phonenumbers.parse(number)
+
+# Printing the country code and national number separately
+country_code = phone_number.country_code
+national_number = phone_number.national_number
+print("Country Code:", country_code)
+print("National Number:", national_number)
+
+# Printing the timezone using the timezone module
+time_zone = timezone.time_zones_for_number(phone_number)
+print("Timezone :", time_zone)
+
+# Printing the geolocation of the given number using the geocoder module
+location = geocoder.description_for_number(phone_number, "en")
+print("Location :", location)
+
+# Printing the service provider name using the carrier module
+service_provider = carrier.name_for_number(phone_number, "en")
+print("Service Provider :", service_provider)
